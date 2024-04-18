6ab622b8bc80d617289e6218ad93319be9db0f9b
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-04-17 23:25:51 -0300

    metodo unidosNoMezclados

--- src/tpeda3/pkg2/Main.java
+++ src/tpeda3/pkg2/Main.java
@@ -27,12 +27,23 @@ public class Main {
 
         Pila a=new Pila(10);
         Pila b=new Pila(10);
+        a.push(1);
+        a.push(2);
+        a.push(300);
+        a.push(400);
+        a.push(500);
         
+        b.push(10);
+        b.push(20);
+        b.push(30);
+        b.push(40);
+        b.push(50);
+
+        Pila.unidosNoMezclados(a, b);
         
-        p1.verElementos();
-        
-        
-        
+//        Esto seria si el metodo no fuera estatico
+//        Pila c=new Pila(10);
+//        c.unidosNoMezclados(a, b);
     }
     
 }
